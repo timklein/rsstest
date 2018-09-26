01735ec189c811487cf1ffc7becd752cca40b41c
Tim Klein <timklein@users.noreply.github.com>
2018-09-26 16:55:15 -0600

    Update template for get regenerative

--- routes/index.js
+++ routes/index.js
@@ -17,12 +17,12 @@ router.get('/', function(req, res, next) {
     const msg = {
       to: process.env.TO_ADDRESS,
       from: process.env.FROM_ADDRESS,
-      subject: 'Your Weekly ' + feed.title + ' Blog Update',
-      html: '<div style="text-align: center;"><a href="http://regenexx.com"><img src="https://d1yoaun8syyxxt.cloudfront.net/sz324-8f36a4ab-9dc3-4663-a0e4-b059c8d42a73-v2" width="500px" align="center"></a><br /><h1 style="color: grey;">This Week\'s Blog Posts from Regenexx</h1><h2 style="color: #0b1423d9;">The latest articles, outcomes, news and commentary on regenerative orthopedic medicine.</h2></div><hr />'
+      subject: 'The Fix! Weekly Blog Updates from ' + feed.title,
+      html: '<div style="text-align: center;"><a href="http://regenexx.com"><img src="https://getregenerative.com/wp-content/uploads/2018/09/newsletter_logo.png" width="500px" align="center"></a><br /><h1 style="color: grey;">This Week\'s Blog Posts from Get Regenerative</h1><h2 style="color: #0b1423d9;">The latest articles, outcomes, news and commentary on regenerative orthopedic medicine.</h2></div><hr />'
     };
 
     
-    for (let index = 0; index < 3; index++) {
+    for (let index = 0; index < 5; index++) {
       
       const element = feed.items[index];
       
@@ -33,6 +33,7 @@ router.get('/', function(req, res, next) {
       console.log(element.contentSnippet);
       
     }
+    // msg.html will need to be base64 encoded prior to forwarding to Infusionsoft's API
     console.log(msg.html);
     sgMail.send(msg);
     res.render('index', { title: 'Express', output: JSON.stringify(feed)});
